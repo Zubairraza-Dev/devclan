09354962518235605c0d4e42207cd37d00dcac05
Zubairraza-Dev <techify.devs@gmail.com>
2024-10-01 16:48:54 +0500

    working-on-case-study-detail-page

---
+++ src/components/InvolvementCard.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+interface InvolvementCardProps {
+  title: string;
+  icon: any; // Ensure type matches Next.js Image import
+}
+
+const InvolvementCard: React.FC<InvolvementCardProps> = ({ title, icon }) => {
+  return (
+    <div className="w-[225px] h-[230px] shadow-[0_4px_15px_rgba(0,0,0,0.5)] flex flex-col items-center justify-center rounded-xl p-4">
+      <Image src={icon} alt={title} />
+      <p className="mt-4 text-lg font-bold">{title}</p>
+    </div>
+  );
+};
+
+export default InvolvementCard;
--- src/data/caseStudies.ts
+++ src/data/caseStudies.ts
@@ -1,25 +1,97 @@
-import redwaveImage from "@/assets/images/redwaveImage.svg";
-import case1 from "../assets/images/case1.svg"
-import case2 from "../assets/images/case2.svg"
-import case3 from "../assets/images/case3.svg"
-import case4 from "../assets/images/case4.svg"
-import case5 from "../assets/images/case5.svg"
-import case6 from "../assets/images/case6.svg"
+// case card images
+import case1 from "../assets/images/case1.svg";
+import case2 from "../assets/images/case2.svg";
+import case3 from "../assets/images/case3.svg";
+import case4 from "../assets/images/case4.svg";
+import case5 from "../assets/images/case5.svg";
+import case6 from "../assets/images/case6.svg";
+// project main image
+import redwave from "@/assets/images/redwaveAboutImage.svg";
+
+// technologies icon
+import vue from "@/assets/icons/vueIcon.svg";
+import node from "@/assets/icons/nodeIcon.svg";
+import mongo from "@/assets/icons/mongodbIcon.svg";
+import docker from "@/assets/icons/dockerIcon.svg";
+
 export const caseStudies = [
   {
     id: 1,
     title: "",
-    description: "Description for case study 1",
-    image: case1,
+    cardImg: case1,
+    mainImg: redwave,
+    shortDescription: "Description for case study 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris.",
+    problems: ["problem 1", "problem 2", "problem 3", "problem 4"],
+    functionality:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum uctus lorem, in tempus mauris. Donec vulputate non neque placeratmattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum,ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc",
+    technologies: [vue, node, mongo, docker],
     background: "bg-red-800",
   },
-  { id: 2, title: "Case Study 2", description: "Description for case study 2" , image: case2},
-  { id: 3, title: "Case Study 3", description: "Description for case study 3", image : case3 },
-  { id: 4, title: "Case Study 4", description: "Description for case study 4", image : case4 },
-  { id: 5, title: "Case Study 5", description: "Description for case study 5", image : case5 },
-  { id: 6, title: "Case Study 6", description: "Description for case study 6", image : case6 },
-  // { id: 7, title: "Case Study 7", description: "Description for case study 7", image : case7 },
-  // { id: 8, title: "Case Study 8", description: "Description for case study 8", image : case8 },
-
-  // Add more case studies as needed
+  {
+    id: 2,
+    title: "Case Study 2",
+    cardImg: case2,
+    mainImg: redwave,
+    shortDescription: "Description for case study 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris.",
+    problems: ["problem 1", "problem 2", "problem 3", "problem 4"],
+    functionality:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum uctus lorem, in tempus mauris. Donec vulputate non neque placeratmattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum,ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc",
+    technologies: [vue, node, mongo, docker],
+  },
+  {
+    id: 3,
+    title: "Case Study 3",
+    cardImg: case3,
+    mainImg: redwave,
+    shortDescription: "Description for case study 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris.",
+    problems: ["problem 1", "problem 2", "problem 3", "problem 4"],
+    functionality:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum uctus lorem, in tempus mauris. Donec vulputate non neque placeratmattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum,ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc",
+    technologies: [vue, node, mongo, docker],
+  },
+  {
+    id: 4,
+    title: "Case Study 4",
+    cardImg: case4,
+    mainImg: redwave,
+    shortDescription: "Description for case study 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris.",
+    problems: ["problem 1", "problem 2", "problem 3", "problem 4"],
+    functionality:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum uctus lorem, in tempus mauris. Donec vulputate non neque placeratmattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum,ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc",
+    technologies: [vue, node, mongo, docker],
+  },
+  {
+    id: 5,
+    title: "Case Study 5",
+    cardImg: case5,
+    mainImg: redwave,
+    shortDescription: "Description for case study 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris.",
+    problems: ["problem 1", "problem 2", "problem 3", "problem 4"],
+    functionality:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum uctus lorem, in tempus mauris. Donec vulputate non neque placeratmattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum,ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc",
+    technologies: [vue, node, mongo, docker],
+  },
+  {
+    id: 6,
+    title: "Case Study 6",
+    cardImg: case6,
+    mainImg: redwave,
+    shortDescription: "Description for case study 5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris. Donec vulputate non neque placerat mattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum, ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu luctus lorem, in tempus mauris.",
+    problems: ["problem 1", "problem 2", "problem 3", "problem 4"],
+    functionality:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum uctus lorem, in tempus mauris. Donec vulputate non neque placeratmattis. Donec eleifend tempor luctus. Fusce consequat erat ut iaculis luctus. Praesent sodales in velit sed fringilla. Nam turpis ipsum,ultrices vel efficitur vitae, sodales luctus augue. Mauris ornare, ipsum ac convallis interdum, lacus leo finibus urna, ut fermentum ipsum neque eu nunc",
+    technologies: [vue, node, mongo, docker],
+  },
 ];
